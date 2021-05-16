662041954049a86e5d796b3906e788a3be0bb220
kirjai <9858620+kirjai@users.noreply.github.com>
2021-05-16 11:29:35 +0100

    add filtering

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,3 +1,18 @@
+// [label, url] pair
+const filterTags = [
+  ['react', 'react'],
+  ['angular', 'angular'],
+]
+
 module.exports = () => ({
   currentYear: new Date().getFullYear(),
+  filterTags: (data) => {
+    const cleanUrl = data.page.url.replace(/\//g, '')
+    const tags = filterTags.filter(([, url]) => cleanUrl !== url)
+
+    if (data.page.url !== '/') {
+      tags.unshift(['all', ''])
+    }
+    return tags
+  },
 })
